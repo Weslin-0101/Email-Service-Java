7e56a879c7155558e1ee50e009cb60a0afa9d9ef
Wesley Lira <wesleeylirac12@gmail.com>
2023-08-24 14:30:55 -0300

    feat: implemented message queues with RabbitMQ

---
+++ src/main/java/com/api/email/configs/RabbitMQConfig.java
@@ -0,0 +1,23 @@
+package com.api.email.configs;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${spring.rabbitmq.template.default-receive-queue}")
+    private String queueName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
---
+++ src/main/java/com/api/email/consumers/EmailConsumer.java
@@ -0,0 +1,26 @@
+package com.api.email.consumers;
+
+import com.api.email.dtos.EmailDTO;
+import com.api.email.model.EmailEntity;
+import com.api.email.services.EmailService;
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+public class EmailConsumer {
+
+    private final EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.template.default-receive-queue}")
+    public void listen(@Payload EmailDTO emailDTO) {
+        EmailEntity emailEntity = new EmailEntity();
+        BeanUtils.copyProperties(emailDTO, emailEntity);
+        emailService.sendEmail(emailEntity);
+
+        System.out.println("Email Status: " + emailEntity.getStatusEmail().toString());
+    }
+}
