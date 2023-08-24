fe935cc88640e3085dcad44fc898d31f0f98b9ae
Wesley Lira <wesleeylirac12@gmail.com>
2023-08-23 21:40:36 -0300

    feat: implemented Domain Layer

---
+++ src/main/java/com/api/email/model/EmailEntity.java
@@ -0,0 +1,30 @@
+package com.api.email.model;
+
+import com.api.email.enums.StatusEmail;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "_email")
+public class EmailEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+}
